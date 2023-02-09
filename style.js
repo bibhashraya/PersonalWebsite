cfae4e1a0315486dda5088557ea5c6360c2560e7
bibhashraya <np03cs4a220108@heraldcollege.edu.np>
2023-02-09 16:47:09 +0545

    js

--- style.js
+++ style.js
@@ -1,2 +1,19 @@
 //taking user to top
 const toTop=()=> window.scrollTo({ top: 0, behavior:"smooth" });
+
+
+
+jQuery(function($) {
+
+    $('.hamburger-menu').click(function() {
+            $('.hamburger-menu').toggleClass('active');
+            $('.menu-overlay').toggleClass('open');
+            $('body').toggleClass('nav-active');
+        });
+        $('.menu-overlay').click(function() {
+            $('.hamburger-menu').removeClass('active');
+            $('.menu-overlay').removeClass('open');
+            $('body').removeClass('nav-active');
+        });
+    
+    } );
